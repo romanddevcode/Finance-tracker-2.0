2e75a3e8bad0da7860908c4ab156f3af70a2f14d
Kaidziro <hosokson@gmail.com>
2025-06-10 20:57:40 +0200

    Updated

--- vite.config.ts
+++ vite.config.ts
@@ -19,12 +19,12 @@ export default defineConfig({
         theme_color: "#00b894",
         icons: [
           {
-            src: "/src/assets/favicon-32x32.png",
+            src: "/pwa-192x192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "/src/assets/favicon-32x32.png",
+            src: "/pwa-512x512.png",
             sizes: "512x512",
             type: "image/png",
           },
