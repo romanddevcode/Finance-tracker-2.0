bd0b5d7f978ad09da53722bf224db382a59d5ec1
Kaidziro <hosokson@gmail.com>
2025-08-17 12:21:39 +0200

    Fix: small fix

--- src/features/transactions/components/GraphsComponents/MoneyFlow.tsx
+++ src/features/transactions/components/GraphsComponents/MoneyFlow.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import IncomeGraph from "../AnlyticsComp/IncomeGraph";
 import { useAnalyticsData } from "../../hooks/useAnalyticsData";
 import { useTransactions } from "../../hooks/useTransactions";
