15132525cfdfc5042d504a1a08b0838ae790cc66
Kaidziro <hosokson@gmail.com>
2025-08-23 17:50:58 +0200

    Refactor: -Separated logic in TransactionsList, adding TransactionsCard.tsx; -Slightly re-edited TransactionsMain; -Refactored hook useTransactionsMutation, providing more optimization; -Refactored service localTransactionsService.ts; -Refactored type transactionInterface.ts due optimization; -Refactored main TransactionsPage.tsx component.

--- src/features/transactions/services/localTransactionsService.ts
+++ src/features/transactions/services/localTransactionsService.ts
@@ -3,16 +3,21 @@ import type { Transaction } from "../types/transactionInterface";
 
 export const getLocalTransactions = async (): Promise<Transaction[]> => {
   const all = await db.transactions.toArray();
-  console.log("Reading from Dexie LOCAL: ", all);
   return all;
 };
 
-export const addLocalTransaction = async (tx: Transaction) => {
-  console.log("Added to Dexie LOCAL: ", tx);
-  return await db.transactions.add({ ...tx, isSynced: 0 });
+export const addLocalTransaction = async (
+  tx: Transaction
+): Promise<Transaction> => {
+  const id = db.transactions.add({ ...tx });
+  const newTransaction = await db.transactions.get(id);
+  if (!newTransaction) throw new Error("Transaction not found");
+
+  return newTransaction;
 };
 
-export const deleteLocalTransaction = async (id: string) => {
+export const deleteLocalTransaction = async (id: string): Promise<string> => {
   console.log("Deleted from Dexie LOCAL: ", id);
-  return await db.transactions.delete(id);
+  await db.transactions.delete(id);
+  return id;
 };
