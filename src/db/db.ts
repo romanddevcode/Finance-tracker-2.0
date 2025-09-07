3c385b5527d80a40d90723da1a45f0dc421b7b6d
Kaidziro <hosokson@gmail.com>
2025-09-07 15:19:18 +0200

    refactor: Refactored Dexie db due new parameters to limit settings; -added few new parameters such as value, currency, isActivated fields; -installed new library framer-motion for new component ErrorPopup.tsx.

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,11 +1,11 @@
 import Dexie, { type Table } from "dexie";
 import type { Transaction } from "../features/transactions/types/transactionInterface";
-import type { Settings } from "../features/transactions/types/budgetLimit";
+import type { SettingsLimit } from "../features/transactions/types/budgetLimit";
 import type { Goal } from "../features/transactions/types/goalsInterface";
 
 class FinanceDB extends Dexie {
   transactions!: Table<Transaction>;
-  settings!: Table<Settings>;
+  settingsLimit!: Table<SettingsLimit>;
   goals!: Table<Goal>;
 
   constructor() {
@@ -13,7 +13,7 @@ class FinanceDB extends Dexie {
     this.version(1).stores({
       transactions:
         "++id, type, amount, currency, date, description, category, isSynced",
-      settings: "&id",
+      settingsLimit: "&id, value, currency, isActivated",
       goals: "&id, title, targetAmount, currentAmount, currency",
     });
   }
